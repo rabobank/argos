e298efaaaf0a0c4d044e7a5d7f3b7ee390e1237d
m durieux <dreux000@gmail.com>
2019-11-11 10:00:32 +0100

    checked base url

--- argos-test/src/test/java/com/rabobank/argos/test/KarateHelper.java
+++ argos-test/src/test/java/com/rabobank/argos/test/KarateHelper.java
@@ -2,7 +2,9 @@ package com.rabobank.argos.test;
 
 import com.intuit.karate.cucumber.CucumberRunner;
 import com.intuit.karate.cucumber.KarateStats;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 class KarateHelper {
 
     private static final String SERVER_BASEURL = "server.baseurl";
@@ -13,6 +15,7 @@ class KarateHelper {
 
     static KarateStats runKarateTests(Class clazz) {
         Properties properties = Properties.getInstance();
+        log.info("karate base url : {}" , properties.getApiBaseUr());
         System.setProperty(SERVER_BASEURL,properties.getApiBaseUr());
         return CucumberRunner.parallel(clazz, 1, REPORT_DIR);
     }
