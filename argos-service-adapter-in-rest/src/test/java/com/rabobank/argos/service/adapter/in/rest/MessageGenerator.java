093bda27d1b950df7095a5450d39da8362e0acf1
m durieux <dreux000@gmail.com>
2019-11-12 09:16:25 +0100

    disabled test for messageGenerator

--- argos-service-adapter-in-rest/src/test/java/com/rabobank/argos/service/adapter/in/rest/MessageGenerator.java
+++ argos-service-adapter-in-rest/src/test/java/com/rabobank/argos/service/adapter/in/rest/MessageGenerator.java
@@ -1,22 +1,30 @@
 package com.rabobank.argos.service.adapter.in.rest;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabobank.argos.domain.model.KeyPair;
+import com.rabobank.argos.domain.SigningProvider;
+import com.rabobank.argos.domain.SigningProviderImpl;
+import com.rabobank.argos.domain.model.RSAPublicKeyFactory;
+import com.rabobank.argos.service.adapter.in.rest.api.model.RestKeyPair;
 import org.apache.commons.io.IOUtils;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class MessageGenerator {
-
     @Test
-   public void generateValidTestKeyPairJson() throws IOException {
-
-        //SigningProvider signingProvider = new SigningProviderImpl();
+    @Ignore
+    public void generateValidTestKeyPairJson() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
+        SigningProvider signingProvider = new SigningProviderImpl();
         String keyPairJson = IOUtils.toString(MessageGenerator.class.getResourceAsStream("/keypair.json"), "UTF-8");
         ObjectMapper mapper = new ObjectMapper();
-        KeyPair keyPair =  new ObjectMapper().readValue(keyPairJson, KeyPair.class);
-
+        RestKeyPair keyPair = mapper.readValue(keyPairJson, RestKeyPair.class);
+        String keyid = signingProvider.computeKeyId(RSAPublicKeyFactory.instance(keyPair.getPublicKey()));
+        keyPair.setKeyId(keyid);
+        mapper.writeValueAsString(keyPair);
+        System.out.println(mapper.writeValueAsString(keyPair));
     }
 
 }
