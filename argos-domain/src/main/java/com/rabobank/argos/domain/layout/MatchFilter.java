f1fc1da352634974a8d6368f2d2e9cd5a5cb756a
m durieux <dreux000@gmail.com>
2019-12-02 17:04:54 +0100

    fixed broken integration test

--- argos-domain/src/main/java/com/rabobank/argos/domain/layout/MatchFilter.java
+++ argos-domain/src/main/java/com/rabobank/argos/domain/layout/MatchFilter.java
@@ -1,14 +1,12 @@
 package com.rabobank.argos.domain.layout;
 
 import com.rabobank.argos.domain.link.Artifact;
-import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.nio.file.FileSystems;
-import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -23,27 +21,21 @@ public class MatchFilter {
     private DestinationType destinationType;
     private String destinationStepName;
 
-    @Setter(AccessLevel.PRIVATE)
-    @Getter(AccessLevel.PRIVATE)
-    private PathMatcher matcher;
 
     @Builder
     public MatchFilter(String pattern, DestinationType destinationType, String destinationStepName) {
         this.pattern = pattern;
         this.destinationType = destinationType;
         this.destinationStepName = destinationStepName;
-        initializeMatcher();
     }
 
     public boolean matchUri(String uri) {
-        return matcher.matches(Paths.get(uri));
+        return FileSystems.getDefault().getPathMatcher("glob:" + pattern).matches(Paths.get(uri));
     }
 
     public List<Artifact> matches(List<Artifact> productsToVerify) {
         return productsToVerify.stream().filter(artifact -> matchUri(artifact.getUri())).collect(toList());
     }
 
-    private void initializeMatcher() {
-        matcher = FileSystems.getDefault().getPathMatcher("glob:" + pattern);
-    }
+
 }
