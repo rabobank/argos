1138fee9cbd0216c5162f0887796e63b88431f1d
Gerard Borst <gerard.borst@rabobank.nl>
2019-12-05 17:19:45 +0100

    add headers

--- argos-domain/src/test/java/com/rabobank/argos/domain/KeyIdProviderImplTest.java
+++ argos-domain/src/test/java/com/rabobank/argos/domain/KeyIdProviderImplTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2019 Rabobank Nederland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.rabobank.argos.domain;
 
 import com.rabobank.argos.domain.key.KeyIdProviderImpl;
@@ -17,7 +32,7 @@ class KeyIdProviderImplTest {
 
     @Test
     void computeKeyId() throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
-        byte[] decode = Base64.getDecoder().decode(IOUtils.toByteArray(this.getClass().getResourceAsStream("/publickey.txt")));
+        byte[] decode = Base64.getDecoder().decode(IOUtils.toByteArray(this.getClass().getResourceAsStream("/publickey")));
         String keyId = new KeyIdProviderImpl().computeKeyId(RSAPublicKeyFactory.instance(decode));
         assertThat(keyId, is("1aaf91a3f8e540cfd9ebbacd6147d43c76abefc535feefd85592197055bea1c8"));
     }
