7d42c41b84567ba8ab8bf520d778a82932798309
Bart Kors <bart@dukesolutions.nl>
2019-12-05 12:02:28 +0100

    added modify rule verification unit tests

--- argos-service-domain/src/main/java/com/rabobank/argos/service/domain/verification/rules/ModifyRuleVerification.java
+++ argos-service-domain/src/main/java/com/rabobank/argos/service/domain/verification/rules/ModifyRuleVerification.java
@@ -1,5 +1,25 @@
 package com.rabobank.argos.service.domain.verification.rules;
 
+/*-
+ * #%L
+ * Argos Supply Chain Notary
+ * %%
+ * Copyright (C) 2019 Rabobank Nederland
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.rabobank.argos.domain.layout.rule.Rule;
 import com.rabobank.argos.domain.layout.rule.RuleType;
 import com.rabobank.argos.domain.link.Artifact;
@@ -9,6 +29,7 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.groupingBy;
@@ -34,11 +55,14 @@ public class ModifyRuleVerification implements RuleVerification {
                 .collect(groupingBy(Artifact::getUri));
 
         return uriMap.values().stream()
-                .filter(artifacts -> artifacts.size() != 2)
-                .filter(artifacts -> artifacts.get(0).getHash().equals(artifacts.get(1).getHash()))
+                .filter(filterNotValidArtifacts())
                 .map(artifacts -> RuleVerificationResult.notOkay())
                 .findFirst()
                 .orElseGet(() -> RuleVerificationResult.okay(filteredProducts));
+    }
+
+    private Predicate<List<Artifact>> filterNotValidArtifacts() {
+        return artifacts -> artifacts.size() != 2 || artifacts.get(0).getHash().equals(artifacts.get(1).getHash());
     }
 
     @Override
