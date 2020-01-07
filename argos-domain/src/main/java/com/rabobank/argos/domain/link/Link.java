30d5b7af15dc0d9836789d0093f12491ccb57ae2
Bart Kors <bart@dukesolutions.nl>
2020-01-07 10:55:00 +0100

    Copyright to 2020

--- argos-domain/src/main/java/com/rabobank/argos/domain/link/Link.java
+++ argos-domain/src/main/java/com/rabobank/argos/domain/link/Link.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Rabobank Nederland
+ * Copyright (C) 2019 - 2020 Rabobank Nederland
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,8 +20,8 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
