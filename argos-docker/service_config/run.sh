8d6445ef3ea742a0b6c752d4ebb84661d01bf65c
Gerard Borst <gerard.borst@rabobank.nl>
2020-01-30 18:47:44 +0100

    bash installed and in she-bang
    default shell doesn't handle dotted(spring boot) environment variables,
    bash does

--- argos-docker/service_config/run.sh
+++ argos-docker/service_config/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright (C) 2019 - 2020 Rabobank Nederland
 #
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 
 if [ -z "$@" ]; then
 	exec java -jar /argos-service.jar
