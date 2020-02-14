b977fcf04e98adb9340713b239dd6a8e6c52444a
m durieux <dreux000@gmail.com>
2020-02-14 14:28:06 +0100

    refactored current autorization for personal account

--- argos-service/src/main/java/com/rabobank/argos/service/security/PersonalAccountUserDetailsService.java
+++ argos-service/src/main/java/com/rabobank/argos/service/security/PersonalAccountUserDetailsService.java
@@ -25,7 +25,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class PersonalAccountUserDetailsService {
 
     private final PersonalAccountRepository personalAccountRepository;
 
